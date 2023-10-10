6455535a7769d69c9552284601513eb7d4ee962b
zamer <zamer37573@gmail.com>
2023-10-10 12:15:53 +0500

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+from addition import add
+from subtraction import subtract
+from multiply import multiply
+if __name__=="__main__":
+    a=6
+    b=4
+    print(add(a,b))
+    print(subtract(a,b))
+    print(multiply(a,b))
+
+    
